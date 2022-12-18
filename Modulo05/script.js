55a9ffe84ad3d4aaf3049dad2cba5e507577501b
b.sohara.git <b.sohara.git@gmail.com>
2022-12-18 19:09:53 -0300

    feat: adding the object class

---
+++ Modulo05/script.js
@@ -0,0 +1,14 @@
+var usuario1 = {
+    nome: 'Bruno',
+    idade: 26,
+    departamento: 'Tecnologia da Informação'
+};
+
+
+console.log(usuario1);
+console.log(typeof usuario1);
+console.log(usuario1.nome);
+console.log(usuario1.idade);
+
+console.log(typeof usuario1.nome);
+console.log(typeof usuario1.idade);
---
+++ Modulo06/script.js
@@ -0,0 +1,7 @@
+var arr = [5, "Bruno", true, {teste: 1, teste:2}];
+console.log(arr);
+
+var arr2 = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr2);
+
+console.log(arr[1]);
