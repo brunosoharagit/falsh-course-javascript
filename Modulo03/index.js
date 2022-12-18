93cef65d65dd072c9574fef69f1acde7a3f8accd
b.sohara.git <b.sohara.git@gmail.com>
2022-12-18 11:34:19 -0300

    feat: inserting the object and boolean js classes

---
+++ Modulo03/index.js
@@ -0,0 +1,12 @@
+var verdadeiro = true;
+
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+console.log(typeof true);
+
+
+var falso = false;
+
+console.log(falso);
+console.log(typeof falso);
+console.log(typeof false);
---
+++ Modulo04/script.js
@@ -0,0 +1,15 @@
+console.log(nome);
+console.log(numero)
+
+var nome = null;
+var sobrenome;
+
+console.log(nome);
+console.log(sobrenome);
+
+
+nome = "Bruno";
+console.log(nome);
+
+var numero = 5;
+console.log(numero);
