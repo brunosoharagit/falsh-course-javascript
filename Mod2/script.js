01ff6aa554475dc58597436b482955437ae42c9d
brunosoharagit <b.sohara.git@gmail.com>
2022-12-28 15:44:22 -0300

    feat: adding bracket notations part 1 and 2

---
+++ Mod2/script.js
@@ -0,0 +1,14 @@
+// Setup
+const lastName1 = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName1.length - 1]; // Change this line
+console.log(lastLetterOfLastName);
+
+console.log("=============================================================");
+
+const lastName2 = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName2.length - 2]; // Change this line
+console.log(secondToLastLetterOfLastName);
