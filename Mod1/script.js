3fd22ae56e783f9246f517928e7a1f1c19d8fff7
brunosoharagit <b.sohara.git@gmail.com>
2022-12-28 15:30:15 -0300

    feat: adding concatenations

--- Mod1/script.js
+++ Mod1/script.js
@@ -12,3 +12,37 @@ var nome = "Bruno";
 var sobrenome = "Sohara";
 
 var variavelComNomeMuitoLongo = 'variável com nome muito longo';
+
+//1. let
+let nomeDoUsuario = "Bruno";
+console.log(nomeDoUsuario);
+
+nomeDoUsuario = "Geraldo";
+console.log(nomeDoUsuario);
+
+//2. const
+let nomeDeOutraPessoa = "Douglas";
+console.log(nomeDeOutraPessoa);
+
+nomeDeOutraPessoa = "Lais";
+console.log(nomeDeOutraPessoa);
+
+console.log("Como um filósofo nos diz: \"Só sei que nada sei\".");
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+console.log(myStr);
+
+let myAnotherStr = "This is the first sentence. ";
+myAnotherStr += "This is the second sentence.";
+
+console.log(myAnotherStr);
+
+const myName = "Bruno";
+const my100thStr = "My name is " + myName + " and I am very well!";
+console.log(my100thStr);
+
+// Change code below this line
+const someAdjective = "too awesome";
+let my500thStr = "Learning to code is ";
+my500thStr += someAdjective;
+console.log(my500thStr);
