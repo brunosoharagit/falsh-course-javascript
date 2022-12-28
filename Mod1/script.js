6303da3fa115ab4c993cf1a2a8b888a90399d684
brunosoharagit <b.sohara.git@gmail.com>
2022-12-28 13:53:46 -0300

    feat: inserting the variable type classes

--- Mod1/script.js
+++ Mod1/script.js
@@ -1,2 +1,14 @@
+//variáveis
 var meuNome = 'Bruno';
 console.log(meuNome);
+
+var a;
+console.log(a);
+
+a = 7;
+console.log(a);
+
+var nome = "Bruno";
+var sobrenome = "Sohara";
+
+var variavelComNomeMuitoLongo = 'variável com nome muito longo';
