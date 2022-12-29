6748756c76d76aa3df7fb955d3e75adff03c8705
brunosoharagit <b.sohara.git@gmail.com>
2022-12-29 15:46:53 -0300

    fix

--- ModPOO/script.js
+++ ModPOO/script.js
@@ -3,9 +3,3 @@ const Distribuidora = class Distribuidora {
     this.nome = nome;
   }
 }
-
-const cpfl1 = new Distribuidora("CPFL Paulista");
-const elektro1 = new Distribuidora("Elektro");
-
-console.log(cpfl1);
-console.log(elektro1);
