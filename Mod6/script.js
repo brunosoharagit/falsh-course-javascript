71f6b36e25fa220d3912290324f44234e7269109
b.sohara.git <b.sohara.git@gmail.com>
2022-12-31 12:46:38 -0300

    feat: inserting function js class codes

---
+++ Mod6/script.js
@@ -0,0 +1,26 @@
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below (abaixo) this line
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above (acima) this line
+
+addThree();
+addFive();
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
