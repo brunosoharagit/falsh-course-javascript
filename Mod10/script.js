e97c63963235c56d1f95002b04c6c6ac3b820904
b.sohara.git <b.sohara.git@gmail.com>
2022-12-31 22:06:41 -0300

    feat: if else context

---
+++ Mod10/script.js
@@ -0,0 +1,22 @@
+//#region 1. if/else intro
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+//#endregion
+
+
+
