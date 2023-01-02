499199caf0370c084980cefe4f5496a376af6df0
b.sohara.git <b.sohara.git@gmail.com>
2023-01-02 20:28:55 -0300

    feat: insert object classes

---
+++ Mod14/script.js
@@ -0,0 +1,35 @@
+//#region 1. introduction to Objects
+const myDog = {
+  // Only change code below this line
+  name: "Billy",
+  legs: 4,
+  tails: 1,
+  friends: ["Flop", "Mandy", "Luke", "Potato"]
+
+  // Only change code above this line
+};
+
+console.log(myDog.friends);
+//#endregion
+
+//#region 2. some random things
+console.log("Depois de encher o bucho de comida, cá estou de volta! :)");
+//#endregion
+
+//#region 3. object property's access
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+console.log(hatValue);
+console.log(shirtValue);
+//#endregion
+
+
