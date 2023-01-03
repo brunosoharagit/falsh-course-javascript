819fb6d612a8b66adeddb6f2df52ec1a6628e36e
brunosoharagit <b.sohara.git@gmail.com>
2023-01-03 11:28:37 -0300

    feat: adding more object exercise and classes

--- Mod14/script.js
+++ Mod14/script.js
@@ -32,4 +32,52 @@ console.log(hatValue);
 console.log(shirtValue);
 //#endregion
 
+//#region 4. object data access through brackets
+// Setup
+const testObj2 = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
 
+// Only change code below this line
+const entreeValue = testObj2["an entree"];   // Change this line
+const drinkValue = testObj2["the drink"];    // Change this line
+//#endregion
+
+//#region 5. object data access through variables
+// Setup
+const testObj3 = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj3[playerNumber];   // Change this line
+//#endregion
+
+//#region 6. object data update
+// Setup
+const myDog1 = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog1["name"] = "Happy Coder";
+//#endregion
+
+//#region 7. add new object data variable
+const myDog2 = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog2.bark = "woof";
+//#endregion
