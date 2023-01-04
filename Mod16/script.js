6da1e4cf4e54deeef3929bacffdcde22b0cec34a
b.sohara.git <b.sohara.git@gmail.com>
2023-01-03 21:01:06 -0300

    feat: inserting iterator js classes

---
+++ Mod16/script.js
@@ -0,0 +1,22 @@
+//#region 1. iterators - while
+// Setup
+const myArray1 = [];
+
+// Only change code below this line
+let i = 5;
+while (i >= 0) {
+  myArray1.push(i);
+  i--;
+}
+//#endregion
+
+//#region 2. iterators - for
+// Setup
+const myArray2 = [];
+
+// Only change code below this line
+let j = 0;
+for(j = 1; j <= 5; j++) {
+  myArray2.push(j);
+}
+//#endergion
