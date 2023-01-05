0a75ac36ec75f6d158582961cb77082be64cbbc4
brunosoharagit <b.sohara.git@gmail.com>
2023-01-05 14:25:02 -0300

    feat: inserting math methods

---
+++ Mod20/script.js
@@ -0,0 +1,33 @@
+//#region 1. Math.random()
+function randomFraction() {
+
+  // Only change code below this line
+  const randomic = Math.random();
+  return randomic;
+
+  // Only change code above this line
+}
+//#endregion
+
+//#region 2. Math.floor()
+function randomWholeNum() {
+
+  // Only change code below this line
+  const randomWholeNum = Math.floor(Math.random() * 10);
+  return randomWholeNum;
+}
+//#endregion
+
+//#region 3. Random Whole Number - resolution
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  const randomRange = Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+
+  return randomRange;
+  // Only change code above this line
+}
+//#endregion
+
+//#region 4.
+
+//#endregion
